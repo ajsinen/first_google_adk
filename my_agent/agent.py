8989a63149f942dce715a127ae371a977dcaf2e6
AldousSinen <aldous.sinen@fortress-asya.com>
2026-05-15 15:15:19 +0800

    update tool description and llm instruction

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -6,28 +6,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 # Mock tool implementation
 def get_current_time(city: str) -> dict:
     """Returns the current time in a specified city."""
     return {"status": "success", "city": city, "time": "10:30 AM"}
 
 
-def get_cat_facts(number: Optional[int]) -> dict:
-    """Returns a random facts about cats everytime. Use the parameter {number} if user ask for more than 1 fact"""
+def get_cat_facts(number: int = 1) -> dict:
+    """Returns a random facts about cats everytime.
+    Args:
+        number: Number of cat facts to return. Defaults to 1
+    """
     cat_url = os.getenv("CAT_API_BASE_URL")
-    if number:
-        cat_url += f"?count={number}"
+    print("cat url without param", cat_url)
+    if number > 1:
+        cat_url += f"?count={str(number)}"
     print("cat url ", cat_url)
     response = requests.get(cat_url)
     message = response.json()
 
-    return {"fact": message.get(["data"][0])}
+    return dict(message)
 
 
 root_agent = Agent(
-    model='gemini-flash-latest',
+    model='gemini-2.5-flash',
     name='root_agent',
     description="Tells the current time in a specified city.",
-    instruction="You are a helpful assistant that tells the current time in cities. Use the 'get_current_time' tool for this purpose.",
+    instruction="You are a helpful assistant that gives what the user wants. Use the tool for the user inquiry "
+                "if possible",
     tools=[get_current_time, get_cat_facts],
 )
